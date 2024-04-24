5f62bb63569c7024ce813c646b051090dd76814f
adamd <adam.d.hinton@gmail.com>
2024-04-24 19:51:15 -0400

    dist bw dir and file

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lpa;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // aka the cwd
+        Path path = Path.of("");
+        System.out.println("cwd = " + path.toAbsolutePath());
+
+        try (Stream<Path> paths = Files.list(path)) {
+            paths
+                    .map(Main::listDir)
+                    .forEach(System.out::println);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
+    private static String listDir (Path path){
+        try{
+            boolean isDir = Files.isDirectory(path);
+            FileTime dateField = Files.getLastModifiedTime(path);
+            return "%s %-15s %s".formatted(dateField, (isDir ? "<DIR>" : ""), path);
+        }
+        catch(IOException e){
+            System.out.println("Whoops! Something went wrong with " + path);
+            return path.toString();
         }
     }
 }
